80299a124c5ae3bd228be64a751bcbb76341ab99
Dell_Liu <liu339251@163.com>
2022-01-08 14:21:26 +0800

    第一次提交

---
+++ hello.py
@@ -0,0 +1,19 @@
+print("你好吗")
+print("好好学习")
+name = input("请输入你的姓名：")
+print("你的姓名是：" + name)
+if name == "good":
+    print("输入正确")
+else:
+    print("输入错误")
+
+
+def add(a, b):
+    return int(a) * int(b)
+
+
+num1 = input("第一个数：")
+num2 = input("第二个数：")
+print(add(num1, num2))
+
+print("测试")
